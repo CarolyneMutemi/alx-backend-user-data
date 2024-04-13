6a76a470b9891988430cac0b5a21c135f2810031
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-04-14 01:44:12 +0300

    Check valid password.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     Returns a salted, hashed password.
     """
     return bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates the provided password matches the hashed password.
+    """
+    return bcrypt.checkpw(str.encode(password), hashed_password)

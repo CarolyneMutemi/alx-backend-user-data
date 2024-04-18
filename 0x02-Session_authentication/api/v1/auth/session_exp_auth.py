c53d0e109333d0f5d06324ee35eb9875ce70f08f
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-04-18 18:13:34 +0300

    Commenting on a method.

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -31,6 +31,9 @@ class SessionExpAuth(SessionAuth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Get user_id from session_id
+        """
         if session_id is None or not isinstance(session_id, str):
             return None
         if session_id not in self.user_id_by_session_id:

39dabb7a4836b9d2c5bf20f34cc721c52f5751eb
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-04-24 22:31:47 +0300

    Typing return value.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,7 +62,7 @@ def logout():
 
 
 @app.route('/profile', strict_slashes=False)
-def profile():
+def profile() -> str:
     """
     Gets a user's profile.
     """
@@ -74,4 +74,4 @@ def profile():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(host="0.0.0.0", port="5000")

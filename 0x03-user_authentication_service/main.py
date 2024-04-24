73f39a338625ab101d4bcc078c634aabb6f3af26
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-04-25 02:04:13 +0300

    Integration tests.

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -5,19 +5,23 @@ Integration testing.
 import requests
 from app import AUTH
 
+
 def register_user(email: str, password: str) -> None:
     """
     Tests the registering user endpoint.
     """
     payload = {"email": email, "password": password}
-    response = requests.post('http://localhost:5000/users', data=payload, timeout=5)
+    response = requests.post('http://localhost:5000/users',
+                             data=payload, timeout=5)
     assert response.status_code == 200
     assert response.json() == {"email": email, "message": "user created"}
-    second_query = requests.post('http://localhost:5000/users', data=payload, timeout=5)
+    second_query = requests.post('http://localhost:5000/users',
+                                 data=payload, timeout=5)
     assert second_query.status_code == 400
     assert second_query.json() == {"message": "email already registered"}
     new_payload = {"email": email, "password": "new_password"}
-    third_query = requests.post('http://localhost:5000/users', data=new_payload, timeout=5)
+    third_query = requests.post('http://localhost:5000/users',
+                                data=new_payload, timeout=5)
     assert third_query.status_code == 400
     assert second_query.json() == {"message": "email already registered"}
 
@@ -27,7 +31,8 @@ def log_in_wrong_password(email: str, password: str) -> None:
     Testing a log in with a wrong password.
     """
     payload = {"email": email, "password": password}
-    response = requests.post('http://localhost:5000/sessions', data=payload)
+    response = requests.post('http://localhost:5000/sessions',
+                             data=payload, timeout=5)
     assert response.status_code == 401
 
 
@@ -36,7 +41,8 @@ def log_in(email: str, password: str) -> str:
     Tests the login endpoint.
     """
     payload = {"email": email, "password": password}
-    response = requests.post('http://localhost:5000/sessions', data=payload)
+    response = requests.post('http://localhost:5000/sessions',
+                             data=payload, timeout=5)
     assert response.status_code == 200
     assert response.json() == {"email": email, "message": "logged in"}
     return response.cookies.get('session_id')
@@ -56,7 +62,8 @@ def profile_logged(session_id: str) -> None:
     """
     user = AUTH.get_user_from_session_id(session_id)
     cookies = {"session_id": session_id}
-    response = requests.get('http://localhost:5000/profile', cookies=cookies, timeout=5)
+    response = requests.get('http://localhost:5000/profile',
+                            cookies=cookies, timeout=5)
     if not user:
         assert response.status_code == 403
         return
@@ -70,11 +77,13 @@ def log_out(session_id: str) -> None:
     """
     cookies = {"session_id": session_id}
     response = requests.delete('http://localhost:5000/sessions',
-                               allow_redirects=True, cookies=cookies, timeout=5)
+                               allow_redirects=True,
+                               cookies=cookies, timeout=5)
     assert response.status_code == 200
     assert response.history[0].status_code == 302
     assert response.json() == {'message': 'Bienvenue'}
-    response = requests.delete('http://localhost:5000/sessions', cookies=cookies, timeout=5)
+    response = requests.delete('http://localhost:5000/sessions',
+                               cookies=cookies, timeout=5)
     assert response.status_code == 403
 
 
@@ -82,7 +91,8 @@ def reset_password_token(email: str) -> str:
     """
     Tests the reset password token endpoint.
     """
-    response = requests.post('http://localhost:5000/reset_password', data={"email": email}, timeout=5)
+    response = requests.post('http://localhost:5000/reset_password',
+                             data={"email": email}, timeout=5)
     response_dict = response.json()
     assert response.status_code == 200
     assert response_dict.get('email') == email
@@ -93,8 +103,10 @@ def update_password(email: str, reset_token: str, new_password: str) -> None:
     """
     Tests the update password endpoint.
     """
-    payload = {"email": email, "reset_token": reset_token, "new_password": new_password}
-    response = requests.put('http://localhost:5000/reset_password', data=payload, timeout=5)
+    payload = {"email": email, "reset_token": reset_token,
+               "new_password": new_password}
+    response = requests.put('http://localhost:5000/reset_password',
+                            data=payload, timeout=5)
     assert response.status_code == 200
     assert response.json() == {"email": email, "message": "Password updated"}
 
